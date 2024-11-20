b0951ebbdcf12f9b072650e29ce18fa400532c13
d33bs <dave.bunten@cuanschutz.edu>
2024-11-19 17:01:01 -0700

    updates for processing

--- src/connectivity_search_PathCount_table/get_tables.py
+++ src/connectivity_search_PathCount_table/get_tables.py
@@ -20,6 +20,9 @@
 # The archive is available under https://zenodo.org/records/3978766 .
 # Only the `PathCount` Table is needed in order to extract single metapaths
 # at a time (needed for other work).
+#
+# Additionally, we extract a `Node` table to help associate `identifier`
+# with `id` (internal versus external labels for data).
 
 # +
 import gzip
@@ -51,7 +54,7 @@ expected_table_count = 15
 
 # table which is targeted within the sql archive above
 target_pathcount_table_name = "public.dj_hetmech_app_pathcount"
-target_identifier_table_name = "public.public.dj_hetmech_app_node"
+target_identifier_table_name = "public.dj_hetmech_app_node"
 
 # duckdb filename
 duckdb_filename = "data/connectivity-search.duckdb"
@@ -89,49 +92,91 @@ with gzip.open(sql_file, "rt") as f:
             if count == expected_table_count:
                 break
 
-# gather the create table statement
+# gather the create table statement for path count table
 extract_and_write_sql_block(
     sql_file=sql_file,
     sql_start=f"CREATE TABLE {target_pathcount_table_name}",
     sql_end=";",
     output_file=(
-        create_table_file := f"create_table.{target_pathcount_table_name}.sql"
+        create_pathcount_table_file := f"create_table.{target_pathcount_table_name}.sql"
+    ),
+)
+
+# gather the create table statement for node table
+extract_and_write_sql_block(
+    sql_file=sql_file,
+    sql_start=f"CREATE TABLE {target_identifier_table_name}",
+    sql_end=";",
+    output_file=(
+        create_identifier_table_file := f"create_table.{target_identifier_table_name}.sql"
     ),
 )
 
 # +
 # show the create table statement
-with open(create_table_file, "r") as create_file:
-    create_sql = "".join(create_file.readlines())
+with open(create_pathcount_table_file, "r") as pathcount_file:
+    pathcount_table_sql = "".join(pathcount_file.readlines())
 
-print(create_sql)
+print(pathcount_table_sql)
+
+# +
+# show the create table statement
+with open(create_identifier_table_file, "r") as identifier_file:
+    identifier_table_sql = "".join(identifier_file.readlines())
+
+print(identifier_table_sql)
 # -
 
-# gather the data for the table
+# gather the data for the path count table
 extract_and_write_sql_block(
     sql_file=sql_file,
     sql_start=f"COPY {target_pathcount_table_name}",
     sql_end="\\.",
-    output_file=(copy_data_file := f"copy_data.{target_pathcount_table_name}.sql"),
+    output_file=(
+        copy_pathcount_data_file := f"copy_data.{target_pathcount_table_name}.sql"
+    ),
 )
 
-# replace the first and last lines of the copy file
+# gather the data for the identifier table
+extract_and_write_sql_block(
+    sql_file=sql_file,
+    sql_start=f"COPY {target_identifier_table_name}",
+    sql_end="\\.",
+    output_file=(
+        copy_identifier_data_file := f"copy_data.{target_identifier_table_name}.sql"
+    ),
+)
+
+# replace the first and last lines of the copy files
 # as these are the header and data termination lines
 # which have no actual values.
-copy_data_file = remove_first_and_last_line_of_file(target_file=copy_data_file)
-copy_data_file
+copy_pathcount_data_file = remove_first_and_last_line_of_file(
+    target_file=copy_pathcount_data_file
+)
+copy_identifier_data_file = remove_first_and_last_line_of_file(
+    target_file=copy_identifier_data_file
+)
+print(copy_pathcount_data_file, copy_identifier_data_file)
 
-# create the table
+# create the path count and identifier tables
 with duckdb.connect(duckdb_filename) as ddb:
-    ddb.execute(create_sql.replace("public.", ""))
+    ddb.execute(pathcount_table_sql.replace("public.", ""))
+    ddb.execute(identifier_table_sql.replace("public.", "").replace("jsonb", "json"))
 
-# # copy the data from the file to duckdb database
-# as tab-delimited file.
+# # copy the data from the files to duckdb database
+# using tab-delimited files.
 with duckdb.connect(duckdb_filename) as ddb:
     ddb.execute(
         f"""
-        COPY dj_hetmech_app_pathcount
-        FROM '{copy_data_file}'
+        COPY {target_pathcount_table_name.replace('public.','')}
+        FROM '{copy_pathcount_data_file}'
+        (DELIMITER '\t', HEADER false);
+        """
+    )
+    ddb.execute(
+        f"""
+        COPY {target_identifier_table_name.replace('public.','')}
+        FROM '{copy_identifier_data_file}'
         (DELIMITER '\t', HEADER false);
         """
     )
@@ -141,14 +186,31 @@ with duckdb.connect(duckdb_filename) as ddb:
 with duckdb.connect(duckdb_filename) as ddb:
     tbl_pathcount = ddb.execute(
         """
-        SELECT *
-        FROM dj_hetmech_app_pathcount
+        SELECT
+            pathcount.id,
+            pathcount.path_count,
+            pathcount.dwpc,
+            pathcount.p_value,
+            pathcount.metapath_id,
+            pathcount.source_id,
+            source.identifier AS source_identifier,
+            pathcount.target_id,
+            target.identifier AS target_identifier,
+            pathcount.dgp_id
+        FROM
+            dj_hetmech_app_pathcount as pathcount
+        LEFT JOIN
+            dj_hetmech_app_node AS source ON
+                pathcount.source_id = source.id
+        LEFT JOIN
+            dj_hetmech_app_node AS target ON
+                pathcount.target_id = target.id;
         """
     ).arrow()
 
 parquet.write_table(
     table=tbl_pathcount,
-    where="data/dj_hetmech_app_pathcount.parquet",
+    where="data/dj_hetmech_app_pathcount_with_identifiers.parquet",
     # compress with zstd for higher compression than snappy
     compression="zstd",
 )
